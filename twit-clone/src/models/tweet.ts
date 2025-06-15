f24d52e0b0e7af0cb0a15ec03fae361d2ec6f855
Yuri Kravets <kravets.yuri.v@gmail.com>
2025-06-15 19:35:32 +0200

    add uuid

--- twit-clone/src/models/tweet.ts
+++ twit-clone/src/models/tweet.ts
@@ -1,35 +1,13 @@
-
-import mongoose, { model, models, Schema } from "mongoose";
-import { v4 as uuidv4 } from "uuid";
+import mongoose, { Schema } from 'mongoose';
 
 const TweetSchema = new Schema({
-    id: {
+    _id: {
+        type: String, // UUID вместо ObjectId
+        required: true,
+    },
+    text: {
         type: String,
         required: true,
-        unique: true,
-        default: uuidv4, // автоматическая генерация UUID
-    },
-    title: {
-        type: String,
-        required: true,
-        maxlength: 100,
-    },
-    body: {
-        type: String,
-        required: true,
-        maxlength: 280,
-    },
-    userId: {
-        type: Number,
-        required: true,
-    },
-    tags: {
-        type: [String],
-        default: [],
-    },
-    reactions: {
-        likes: { type: Number, default: 0 },
-        dislikes: { type: Number, default: 0 },
     },
     createdAt: {
         type: Date,
@@ -37,7 +15,43 @@ const TweetSchema = new Schema({
     },
 });
 
-export const Tweet = models.Tweet || model('Tweet', TweetSchema);
+const Tweet = mongoose.models.Tweet || mongoose.model('Tweet', TweetSchema);
+
+export default Tweet;
+// import mongoose, { model, models, Schema } from "mongoose";
+//
+//
+// const TweetSchema = new Schema({
+//
+//     title: {
+//         type: String,
+//         required: true,
+//         maxlength: 100,
+//     },
+//     body: {
+//         type: String,
+//         required: true,
+//         maxlength: 280,
+//     },
+//     userId: {
+//         type: Number,
+//         required: true,
+//     },
+//     tags: {
+//         type: [String],
+//         default: [],
+//     },
+//     reactions: {
+//         likes: { type: Number, default: 0 },
+//         dislikes: { type: Number, default: 0 },
+//     },
+//     createdAt: {
+//         type: Date,
+//         default: Date.now,
+//     },
+// });
+//
+// export const Tweet = models.Tweet || model('Tweet', TweetSchema);
 // const TweetSchema = new Schema({
 //     title: {
 //         type: String,
